b77d2bb8677fa5a23b69ea25d59792a448101e0b
Andrew <papsujevicandrew@gmail.com>
2023-11-08 21:12:07 +0200

    setup init
    removed template misc,
    added normalizing, fonts.
    added basic npm packages

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
+import GlobalStyles from 'styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <GlobalStyles />
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
